3544969a57ea02e0640d19d40f41320b886feb03
jackDenial <jatin.bhalla@atmecs.com>
2018-08-30 10:37:36 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Jatin!";
+    private final String message = "Hello Jatin bhalla!";
 
     public App() {}
 
